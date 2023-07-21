45c0402bf6434b74c0876822abb69191a994cfe4
MariaRuaBF <maria.rua@blankfactor.com>
2023-07-21 08:48:08 -0500

    Create functionality for filter per categories

--- src/components/emojiPicker/EmojiPickerContainer.tsx
+++ src/components/emojiPicker/EmojiPickerContainer.tsx
@@ -11,21 +11,6 @@ const EmojiPickerContainer = forwardRef<HTMLInputElement>((_, ref) => {
   const [emojis, setEmojis] = useState<EmojiType[]>(emojiList);
   const [frequentlyEmojis, setFrequentlyEmojis] = useState<EmojiType[]>([]);
 
-  function handleSearch(e: ChangeEvent<HTMLInputElement>) {
-    const query: string = e.target.value.toLowerCase();
-    if (query) {
-      const search = emojiList.filter((emoji) => {
-        return (
-          emoji.name.toLowerCase().includes(query) ||
-          emoji.keywords.toLowerCase().includes(query)
-        );
-      });
-      setEmojis(search);
-    } else {
-      setEmojis(emojiList);
-    }
-  }
-
   useEffect(() => {
     const frequentlyUsedEmojisString =
       localStorage.getItem("frequently emojis");
@@ -48,6 +33,33 @@ const EmojiPickerContainer = forwardRef<HTMLInputElement>((_, ref) => {
     }
   }, [frequentlyEmojis]);
 
+  const categories: string[] = [];
+  emojiList.forEach((emoji: EmojiType) => {
+    const { category } = emoji;
+    if (!categories.includes(category)) {
+      categories.push(category);
+    }
+  });
+
+  function handleSearch(e: ChangeEvent<HTMLInputElement>) {
+    const query: string = e.target.value.toLowerCase();
+    if (query) {
+      const search = emojiList.filter((emoji) => {
+        return (
+          emoji.name.toLowerCase().includes(query) ||
+          emoji.keywords.toLowerCase().includes(query)
+        );
+      });
+      setEmojis(search);
+    } else {
+      setEmojis(emojiList);
+    }
+  }
+
+  function emojiPerCategory(category: string) {
+    return emojis.filter((emoji) => emoji.category == category);
+  }
+
   function handleOnClickEmoji(emoji: EmojiType) {
     if (ref && typeof ref !== "function" && ref.current) {
       const cursorPosition = ref.current.selectionStart || 0;
@@ -66,31 +78,37 @@ const EmojiPickerContainer = forwardRef<HTMLInputElement>((_, ref) => {
   }
 
   return (
-    <div className="absolute w-96 h-80 border-solid border-2 border-white bg-black opacity-75 flex flex-col p-4 box-border rounded-lg shadow-xl gap-4 ">
+    <div className="absolute w-96 h-80 border-solid border-2 border-white bg-black opacity-75 flex flex-col p-4 box-border rounded-lg shadow-xl ">
       <EmojiSearch onSearch={handleSearch} />
-      <div className="text-white uppercase flex flex-col">
-        <span>Frequently used</span>
-        <div className="flex gap-0.5 flex-wrap">
-          {frequentlyEmojis.map((emoji) => (
-            <EmojiButton
-              key={emoji.symbol}
-              emoji={emoji}
-              onClick={handleOnClickEmoji}
-            />
-          ))}
+      <div className="w-fit overflow-scroll py-2 scroll-smooth ">
+        <div className="text-white uppercase flex flex-col">
+          <span>Frequently used</span>
+          <div className="flex gap-0.5 flex-wrap">
+            {frequentlyEmojis.map((emoji) => (
+              <EmojiButton
+                key={emoji.symbol}
+                emoji={emoji}
+                onClick={handleOnClickEmoji}
+              />
+            ))}
+          </div>
         </div>
-      </div>
-      <div>
-        <span className="text-white uppercase flex flex-col">
-          Smileys & people
-        </span>
-        <div className="flex gap-0.5 flex-wrap">
-          {emojis.map((emoji) => (
-            <EmojiButton
-              key={emoji.symbol}
-              emoji={emoji}
-              onClick={handleOnClickEmoji}
-            />
+        <div>
+          {categories.map((category, index) => (
+            <div key={index} className="my-2">
+              <span className="text-white uppercase flex flex-col">
+                {category}
+              </span>
+              <div className="flex gap-0.5 flex-wrap">
+                {emojiPerCategory(category).map((emoji) => (
+                  <EmojiButton
+                    key={emoji.symbol}
+                    emoji={emoji}
+                    onClick={handleOnClickEmoji}
+                  />
+                ))}
+              </div>
+            </div>
           ))}
         </div>
       </div>
