4d155288a1ab123cea7b8e87a265ddf6d73fbf92
MariaRuaBF <maria.rua@blankfactor.com>
2023-07-24 11:23:18 -0500

    styled components in React with Typescript

--- src/components/Div.tsx
+++ src/components/Div.tsx
@@ -1,37 +1,64 @@
-import { ReactNode, useEffect } from "react";
-
-interface DataProps {
-  id: string;
-  body: string;
-  title: string;
-  userId: string;
-}
+import { ReactNode } from "react";
 
 interface DivProps {
   children?: ReactNode;
   id?: string;
   className?: string;
+  title?: string;
+  accessKey?: string;
   onClick?: (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => void;
+  onChange?: (e: React.FormEvent<HTMLDivElement>) => void;
+  onCopy?: (e: React.ClipboardEvent<HTMLDivElement>) => void;
+  onDoubleClick?: (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => void;
+  onFocus?: (e: React.FocusEvent<HTMLDivElement, Element>) => void;
+  onKeyDown?: (e: React.KeyboardEvent<HTMLDivElement>) => void;
+  onKeyUp?: (e: React.KeyboardEvent<HTMLDivElement>) => void;
+  onMouseMove?: (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => void;
+  onMouseOver?: (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => void;
 }
 
-function loggingIdentity<T, Z>(arg: T): Z {
-  console.log(arg.length);
-  return arg;
-}
+// Generics
+// function loggingIdentity<T, Z>(arg: T): Z {
+//   console.log(arg.length);
+//   return arg;
+// }
 
-export const Div: React.FC<DivProps> = ({ children }) => {
-  useEffect(() => {
-    function responseFetch() {
-      fetch("https://jsonplaceholder.typicode.com/posts")
-        .then((response) => response.json())
-        .then((json) => console.log(json))
-        .catch((e) => console.log(e));
-    }
-    loggingIdentity<string[], string>([]);
-    loggingIdentity<number[], string>([]);
-  }, []);
-  return <div>{children}</div>;
+// loggingIdentity<string[], string>([]);
+// loggingIdentity<number[], string>([]);
+
+export const Div: React.FC<DivProps> = ({
+  children,
+  id,
+  className,
+  title,
+  accessKey,
+  onClick,
+  onChange,
+  onCopy,
+  onDoubleClick,
+  onFocus,
+  onKeyDown,
+  onKeyUp,
+  onMouseMove,
+  onMouseOver,
+}) => {
+  return (
+    <div
+      id={id}
+      title={title}
+      className={className}
+      accessKey={accessKey}
+      onClick={onClick}
+      onChange={onChange}
+      onCopy={onCopy}
+      onDoubleClick={onDoubleClick}
+      onFocus={onFocus}
+      onKeyDown={onKeyDown}
+      onKeyUp={onKeyUp}
+      onMouseMove={onMouseMove}
+      onMouseOver={onMouseOver}
+    >
+      {children}
+    </div>
+  );
 };
-
-// 20 propiedades del elemento div, button, input
-// p, span, h1, input
---
+++ src/components/Img.tsx
@@ -0,0 +1,48 @@
+interface ImgProps {
+  alt?: string;
+  height?: string | number;
+  loading?: "eager" | "lazy";
+  referrerPolicy?: React.HTMLAttributeReferrerPolicy;
+  sizes?: string;
+  src?: string;
+  srcSet?: string;
+  useMap?: string;
+  width?: string | number;
+  onAbort?: (e: React.SyntheticEvent<HTMLImageElement, Event>) => void;
+  onError?: (e: React.SyntheticEvent<HTMLImageElement, Event>) => void;
+  onLoad?: (e: React.SyntheticEvent<HTMLImageElement, Event>) => void;
+}
+
+const Img: React.FC<ImgProps> = ({
+  alt,
+  height,
+  loading,
+  referrerPolicy,
+  sizes,
+  src,
+  srcSet,
+  useMap,
+  width,
+  onAbort,
+  onError,
+  onLoad,
+}) => {
+  return (
+    <img
+      alt={alt}
+      height={height}
+      loading={loading}
+      referrerPolicy={referrerPolicy}
+      sizes={sizes}
+      src={src}
+      srcSet={srcSet}
+      useMap={useMap}
+      width={width}
+      onAbort={onAbort}
+      onError={onError}
+      onLoad={onLoad}
+    />
+  );
+};
+
+export default Img;
